84a2d52317eff6ccb1adf492d47ec363842bab96
shahid <shahidazam574@gmail.com>
2023-06-18 10:59:34 +0530

    addedd config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { globalMongoConfig } from './config/mongodb-connection';
 
 
-const globalMongoConfig = () => {
-  const username = encodeURIComponent(process.env.MONGO_DB_USERNAME);
-  const password = encodeURIComponent(process.env.MONGO_DB_PASSWORD);
-  const DB_NAME = process.env.MONGO_DB_NAME;
-  const DB_HOST = process.env.MONGO_DB_HOST;
-  const AUTH = `${username}:${password}`;
-  const DB = `${DB_HOST}/${DB_NAME}`;
-  const MONGO_URI = `mongodb+srv://${AUTH}@${DB}?retryWrites=true&w=majority`;
 
-  return {
-    MONGO_URI,
-  };
-};
 
 
 @Module({
