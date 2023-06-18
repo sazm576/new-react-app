b03712b833d86d36992d76a37aa7dd1b40819787
shahid <shahidazam574@gmail.com>
2023-06-18 10:46:19 +0530

    added db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+
+const globalMongoConfig = () => {
+  const username = encodeURIComponent(process.env.MONGO_DB_USERNAME);
+  const password = encodeURIComponent(process.env.MONGO_DB_PASSWORD);
+  const DB_NAME = process.env.MONGO_DB_NAME;
+  const DB_HOST = process.env.MONGO_DB_HOST;
+  const AUTH = `${username}:${password}`;
+  const DB = `${DB_HOST}/${DB_NAME}`;
+  const MONGO_URI = `mongodb+srv://${AUTH}@${DB}?retryWrites=true&w=majority`;
+
+  return {
+    MONGO_URI,
+  };
+};
+
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: ['.env.development.local', '.env.development', '.env'],
+  }),
+  MongooseModule.forRoot(globalMongoConfig().MONGO_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Injectable()
+
 export class AppService {
+  constructor(){
+  }
   getHello(): string {
-    return 'Hello World!';
+    return `'Hello World!' ${process.env.MONGO_DB_USERNAME}`;
   }
 }
